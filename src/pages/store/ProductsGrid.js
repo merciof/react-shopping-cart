515ab44ac2b5b2f0e16b8b4df85c4c4af0595758
Mércio Andrade Filho <merciofilho@gmail.com>
2021-10-21 21:11:37 -0300

    remove styles from ProductsGrid

--- src/pages/store/ProductsGrid.js
+++ src/pages/store/ProductsGrid.js
@@ -1,41 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import ProductItem from './ProductItem';
-import styles from './ProductsGrid.module.scss';
-import { useProducts } from '../../hooks/useProducts';
+import ProductItem from "./ProductItem";
+import { useProducts } from "../../hooks/useProducts";
 
-const ProductsGrid = () => {
+function ProductsGrid() {
+  const { products } = useProducts();
 
-    const { products } = useProducts()
+  return (
+    <>
+      <div>{products.length} Products</div>
 
-    return ( 
-        <div className={styles.p__container}>
-            <div className="row">
-                <div className="col-sm-8">
-                    <div className="py-3">
-                        {products.length} Products
-                    </div>
-                </div>
-                <div className="col-sm-4">
-                    <div className="form-group">
-                        <input type="text" name="" placeholder="Search product" className="form-control" id=""/>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.p__grid}>
+      <input type="text" placeholder="Search product" />
 
-                {
-                    products.map(product => (
-                        <ProductItem key={product.id} product={product}/>
-                    ))
-                }
-
-            </div>
-            <div className={styles.p__footer}>
-
-            </div>
-        </div>
-     );
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </>
+  );
 }
- 
+
 export default ProductsGrid;
