f7279a76abcaee966f4ffa92cefbfe8c882e9e38
Mércio Andrade Filho <merciofilho@gmail.com>
2021-10-21 21:07:39 -0300

    remove styles from cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,62 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import CartProducts from './CartProducts';
-import Layout from '../../components/Layout';
-import { useCart } from '../../hooks/useCart';
-import { formatNumber } from '../../helpers/utils';
+import CartProducts from "./CartProducts";
+import Layout from "../../components/Layout";
+import { useCart } from "../../hooks/useCart";
+import { formatNumber } from "../../helpers/utils";
 
-const Cart = () => {
+function Cart() {
+  const {
+    total,
+    cartItems,
+    itemCount,
+    clearCart,
+    checkout,
+    handleCheckout
+  } = useCart();
 
-    const { total, cartItems, itemCount, clearCart, checkout, handleCheckout } = useCart();
-    
-    return ( 
-        <Layout title="Cart" description="This is the Cart page" >
-            <div >
-                <div className="text-center mt-5">
-                    <h1>Cart</h1>
-                    <p>This is the Cart Page.</p>
-                </div>
+  return (
+    <Layout title="Cart" description="This is the Cart page">
+      <h1>Carrinho</h1>
+      <p>Esta é a página do carrinho.</p>
 
-                <div className="row no-gutters justify-content-center">
-                    <div className="col-sm-9 p-3">
-                        {
-                            cartItems.length > 0 ?
-                            <CartProducts/> :
-                            <div className="p-3 text-center text-muted">
-                                Your cart is empty
-                            </div>
-                        }
+      {cartItems.length > 0 ? <CartProducts /> : <div>Your cart is empty</div>}
 
-                        { checkout && 
-                            <div className="p-3 text-center text-success">
-                                <p>Checkout successfull</p>
-                                <Link to="/" className="btn btn-outline-success btn-sm">BUY MORE</Link>
-                            </div>
-                        }
-                    </div>
-                    {
-                        cartItems.length > 0 && 
-                        <div className="col-sm-3 p-3">
-                            <div className="card card-body">
-                                <p className="mb-1">Total Items</p>
-                                <h4 className=" mb-3 txt-right">{itemCount}</h4>
-                                <p className="mb-1">Total Payment</p>
-                                <h3 className="m-0 txt-right">{formatNumber(total)}</h3>
-                                <hr className="my-4"/>
-                                <div className="text-center">
-                                    <button type="button" className="btn btn-primary mb-2" onClick={handleCheckout}>CHECKOUT</button>
-                                    <button type="button" className="btn btn-outlineprimary btn-sm" onClick={clearCart}>CLEAR</button>
-                                </div>
+      {checkout && (
+        <div>
+          <p>Checkout successfull</p>
+          <Link to="/">BUY MORE</Link>
+        </div>
+      )}
 
-                            </div>
-                        </div>
-                    }
-                    
-                </div>
-            </div>
-        </Layout>
-     );
+      {cartItems.length > 0 && (
+        <>
+          <p>Total Items</p>
+          <h4>{itemCount}</h4>
+          <p>Total Payment</p>
+          <h3>{formatNumber(total)}</h3>
+
+          <hr />
+
+          <button type="button" onClick={handleCheckout}>
+            CHECKOUT
+          </button>
+          <button type="button" onClick={clearCart}>
+            CLEAR
+          </button>
+        </>
+      )}
+    </Layout>
+  );
 }
- 
+
 export default Cart;
